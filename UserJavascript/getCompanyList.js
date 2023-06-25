03ff671c3b011340a0a9be914ae7ad187b988a3b
ginger_ale <137699642+gingerale333@users.noreply.github.com>
2023-06-26 01:20:34 +0900

    UserJavascriptで検索結果のタイトル一覧を取得する：次ページへ引き継ぐ動作を削除
    URL長Overのため。説明　https://qiita.com/ginger_ale/items/773122983e9ee2ca4d9c

--- UserJavascript/getCompanyList.js
+++ UserJavascript/getCompanyList.js
@@ -1,12 +1,10 @@
 /**
  * 検索結果として表示中の会社名リストを表示
- * 現在表示している分のみ対象、次のページへの引継ぎを行う
+ * 現在の表示内容のみ対象、OKで次のページへ遷移
  * 
  * Google Chrome 拡張機能「User JavaScript and CSS」使用
  * @author gingerale333
  */
-
-const argname = 'cmpylst';
 
 /**
  * 会社名一覧を表示
@@ -15,7 +13,6 @@ function popupConfirmGetAll() {
     console.info('User Javascript is running.');
 
     //ページ遷移処理
-    var beforeCmpyLst = getBeforeCmpyLst();
     var nextButton = $('.btn_r.last')[0]; //次ページへの遷移ボタンのクラス名
     var nextLink = (nextButton === undefined ? '' : $(nextButton).find('a')[0]);
     var nextPage = (nextLink === undefined ? '' : nextLink.href); //次ページのURL
@@ -29,31 +26,14 @@ function popupConfirmGetAll() {
     });
 
     //表示
-    var nameText = nameArr.join('\r\n') + (beforeCmpyLst === '' ? '' : '\r\n') + beforeCmpyLst;
+    var nameText = nameArr.join('\r\n');
     var msg = 'このページの会社名一覧は下記です。' + (nextPage === '' ? '' : '次ページに遷移しますか');
     var res = prompt(msg, nameText);
 
     //OKを押すと次ページへ遷移
     if (res !== null && nextPage !== '') {
-        location.href = nextPage + '&' + argname + '=' + encodeURIComponent(nameText);
+        location.href = nextPage;
     }
-}
-
-/**
- * 前ページからの遷移の場合は会社名リスト引継ぎ
- * @returns 会社名リスト
- */
-function getBeforeCmpyLst() {
-    const target = argname + '=';
-    var query = location.search;
-    var queryElement = query.split('&');
-    var beforeCmpyLst = '';
-    queryElement.forEach(function (element) {
-        if (element.startsWith(target)) {
-            beforeCmpyLst = decodeURIComponent(element.substring(target.length));
-        }
-    });
-    return beforeCmpyLst;
 }
 
 // ページの読み込み完了時に実行
